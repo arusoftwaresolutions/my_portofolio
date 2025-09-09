731043ef27d9ec93980b8b33750a20e812475267
arusoftwaresolutions <arayahaftu1229@gmail.com>
2025-09-09 11:34:07 +0300

    fixing some errors

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -36,9 +36,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'legal-joceline-arusoftwaresolutions-366e2846.koyeb.app',
-    'www.legal-joceline-arusoftwaresolutions-366e2846.koyeb.app',
     'araya-haftu.duckdns.org',
-    'www.araya-haftu.duckdns.org'
 ]
 
 
@@ -167,8 +165,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://legal-joceline-arusoftwaresolutions-366e2846.koyeb.app",
-    "https://araya-haftu.com",
-    "https://www.araya-haftu.com",
+    "https://araya-haftu.duckdns.org",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -178,8 +175,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://legal-joceline-arusoftwaresolutions-366e2846.koyeb.app",
-    "https://araya-haftu.com",
-    "https://www.araya-haftu.com",
+    "https://araya-haftu.duckdns.org",
 ]
 
 # Logging
