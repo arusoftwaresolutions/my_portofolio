1b9edcd97bcf1e9460f0b74cad9a38b7fc1a1ced
arusoftwaresolutions <arayahaftu1229@gmail.com>
2025-09-09 11:53:51 +0300

    fixing the message submitinng error

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,6 @@
 import json
 import logging
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.core.mail import send_mail
 from django.conf import settings
@@ -11,7 +10,6 @@ from .models import ContactMessage
 logger = logging.getLogger('contact')
 
 
-@csrf_exempt
 @require_http_methods(["POST"])
 def contact_form(request):
     """
